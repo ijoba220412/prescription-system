07996ac6273ce482a7edb5955169723bc9ed75e8
ijoba220412 <95876908+ijoba220412@users.noreply.github.com>
2026-04-29 10:53:56 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-```javascript
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   experimental: {
     optimizePackageImports: ['lucide-react'],
   },
 };
-```
+
+module.exports = nextConfig;
